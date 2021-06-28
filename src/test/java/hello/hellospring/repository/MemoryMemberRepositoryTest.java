f4bd9012592b7e9c36c8625b5c2cf2de6393ab86
kvoyant <kvoyant@gmail.com>
2021-06-28 11:17:27 +0900

    회원 리포지토리 테스트 케이스 작성 - 함수별 테스트 - 전체 테스트 성공

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,48 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();//optional 꺼내는 방법
+//        System.out.println("result = " + (result == member) );
+
+        Assertions.assertEquals(member, result);
+        //Parameter Info via (Command + P)
+        //expected 기대하는것 : member
+
+//        assertThat(member).isEqualTo(result);//error!
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+        //member2 커서상태에서 shift + fn + f6 하면 전체 수정 가능
+
+        Member result = repository.findByName("spring1").get();
+        assertEquals(result, member1);
+    }
+
+}
