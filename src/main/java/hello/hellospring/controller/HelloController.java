532c115784298c43d4f03f199148d12a2e8d2ef4
kvoyant <kvoyant@gmail.com>
2021-06-28 09:08:45 +0900

    API - 테스트

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,15 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+        //http://localhost:8080/hello-string?name=spring!!!!
+        //소스보기 하면 hello spring!!! 만 나오고 ResponseBody 에 그대로 써준다
+        //템플릿 엔진으로 조작이 아니라 그대로 데이터를 내려주는 방식
+    }
+
+    
 }
