b6ecee5353b735b2ad0b50d81df8a2f031286652
kvoyant <kvoyant@gmail.com>
2021-06-28 09:32:39 +0900

    API 방식 - JsonConverter - getter setter , Propertie 접근방식 , 자바빈규약 - 객체반환 - ResponseBody

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -21,6 +21,7 @@ public class HelloController {
         return "hello-template";
     }
 
+    //문자반환
     @GetMapping("hello-string")
     @ResponseBody
     public String helloString(@RequestParam("name") String name) {
@@ -30,5 +31,27 @@ public class HelloController {
         //템플릿 엔진으로 조작이 아니라 그대로 데이터를 내려주는 방식
     }
 
-    
+    //객체반환
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();//cmd + shift + enter (자동완성 종료)
+        hello.setName(name);
+        return hello;
+        //객체가 반환되면 JSON 으로 출력 (JsonConverter) cf:StringConverter
+        //http://localhost:8080/hello-api?name=spring!!!!
+    }
+
+    static class Hello {
+        private String name;
+
+        //getter setter 방식, propertie 접근방식, 자바빈 규약
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
