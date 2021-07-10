12188ebed537ede056629099cdaf46d2ed280154
kvoyant <kvoyant@gmail.com>
2021-07-11 03:29:26 +0900

    Async 비동기 서비스 테스트 - extends , override 하지 않는 Configuration class로 테스트 성공 - http://localhost:8080/async

--- src/main/java/hello/hellospring/AsyncConfig.java
+++ src/main/java/hello/hellospring/AsyncConfig.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
-@Configuration
+//@Configuration
 @EnableAsync
 public class AsyncConfig extends AsyncConfigurerSupport {
 
